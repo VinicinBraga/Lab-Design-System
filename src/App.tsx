9e584b8f7a37a019c2ff87c1f0d0f7dbd61a73f1
VinicinBraga <vinicius@bragamatta.com.br>
2022-10-14 17:34:01 -0300

    Iniciando Projeto e Estruturando  Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+  <div>
+    <h1 className="font-bold text-5xl text-violet-500">Hello World!</h1>
+    <button className=" bg-orange-400 font-medium px-4 py-2 rounded text-white">Enviar</button>
+  </div>
+  )
+}
+
