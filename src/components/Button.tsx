516b26f631b818729473cf80eb62a6a0b24dfc47
VinicinBraga <vinicius@bragamatta.com.br>
2022-10-17 17:24:26 -0300

    Button component with storybook

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,7 +15,7 @@ export function Button({  children, asChild }: ButtonProps) {
   return (
     <Comp
       className={clsx(
-      'py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full hover:bg-cyan-300 focus:ring-2 ring-white'     )}
+      'py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full hover:bg-cyan-300 focus:ring-2 ring-white')}
     >
       {children}
     </Comp>
