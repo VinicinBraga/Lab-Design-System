c0df13417c0fec768abf1912548f562a4fae0bd8
VinicinBraga <vinicius@bragamatta.com.br>
2022-10-18 11:03:43 -0300

    TextInput component with storybook

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ export function App() {
   return (
   <div>
     <h1 className="font-bold text-5xl text-violet-500">Hello World!</h1>
-    <button className=" bg-orange-400 font-medium px-4 py-2 rounded text-white">Enviar</button>
   </div>
   )
 }
